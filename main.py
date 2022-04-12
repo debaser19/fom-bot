92fa4eba5ba334bad9bbeb3628ae0a59aae2eaa1
debaser19 <steve@stoveld.xyz>
2022-04-12 10:33:51 -0400

    deploy to aws

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from pstats import Stats
-from pydoc import describe
 from discord.ext import commands
 import discord
-import requests
 import gspread
-import pandas as pd
-from datetime import datetime
-import json
 
 import config
 
@@ -18,16 +12,24 @@ def get_fom_sheet():
 
 
 class Player:
-    def __init__(self, challonge_name, bnet_tag, discord_tag, race):
-        self.challonge_name = challonge_name
-        self.bnet_tag = bnet_tag
-        self.discord_tag = discord_tag
+    def __init__(self, name, race, wins, losses, win_pct, seasons, s1_champ, s2_champ, s1_mp, s2_mp, total_mp, rank):
+        self.name = name
         self.race = race
+        self.wins = wins
+        self.losses = losses
+        self.win_pct = win_pct
+        self.seasons = seasons
+        self.s1_champ = s1_champ
+        self.s2_champ = s2_champ
+        self.s1_mp = s1_mp
+        self.s2_mp = s2_mp
+        self.total_mp = total_mp
+        self.rank = rank
 
 
 def get_players_list():
     sheet = get_fom_sheet()
-    ws = sheet.worksheet("player api")
+    ws = sheet.worksheet("Bot Stats")
     records = ws.get_all_records()
 
     players = []
@@ -35,50 +37,56 @@ def get_players_list():
     for record in records:
         players.append(
             Player(
-                record.get('challonge name'),
-                record.get('battlenet tag'),
-                record.get('discord name'),
-                record.get('race')
+                record.get('Player'),
+                record.get('Race'),
+                record.get('Wins'),
+                record.get('Losses'),
+                record.get('Win %'),
+                record.get('Seasons'),
+                record.get('S1 CHAMP'),
+                record.get('S2 CHAMP'),
+                record.get('s1 MP'),
+                record.get('s2 MP'),
+                record.get('MP'),
+                record.get('Rank')
             )
         )
     
     return players
 
 
-@bot.command(name='players')
-async def players(ctx:commands.Context):
+@bot.command(name='stats')
+async def stats(ctx:commands.Context, user):
     players = get_players_list()
     for player in players:
-        print(player)
+        if user.lower() == player.name.lower():
+            content_string = f"""
+            **Player Name**: {player.name}
+            **Rank**: {player.rank}
+            **Race**: {player.race}
+            **Wins**: {player.wins}
+            **Losses**: {player.losses}
+            **Win Pct**: {player.win_pct}
+            **Seasons Played**: {player.seasons}
+            **Season 1 Manner Points**: {player.s1_mp}
+            **Season 2 Manner Points**: {player.s2_mp}
+            **Total Manner Points**: {player.total_mp}
+            """
+            embed = discord.Embed(
+                title=f"{player.name} Stats",
+                colour=0x0C2C55,
+                description=content_string
+            )
 
+            fom_logo = 'https://s3.amazonaws.com/challonge_app/organizations/images/000/143/459/large/discord_icon.png'
+            embed.set_author(name='Fountain of Manner', icon_url=fom_logo)
 
-@bot.command(name='s2')
-async def s2(ctx: commands.Context, username=None):
-    worksheet = get_fom_sheet().get_worksheet("player api")
-    if username:
-        if username.lower() in worksheet.get_all_records():
-            for user in worksheet.get_all_records():
-                if username.lower() in user['Player'].lower():
-                    print(f'found {user["Player"]}')
-                else:
-                    print(f'Username not found - {user["Player"]}')
-    else:
-        print('Dumping stats list')
-        df = pd.DataFrame(data=worksheet.get_all_values())
-        df.columns = df.iloc[0]
-        df = df.reindex(df.index.drop(0)).reset_index(drop=True)
-        df.columns.name = None
-        print(df)
+            if player.s1_champ == 'YES':
+                embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/963256852613832734/963258532847845476/trophy_1.png')
+            if player.s2_champ == 'YES':
+                embed.set_thumbnail(url='https://i.imgur.com/txh9BQm.png')
 
-        embed=discord.Embed(
-            title='Season 2 Stats',
-            colour=0xE45B9D,
-            description=df
-        )
-        fom_logo = 'https://s3.amazonaws.com/challonge_app/organizations/images/000/143/459/large/discord_icon.png'
-        embed.set_author(name='Fountain of Manner', icon_url=fom_logo)
-
-        await ctx.reply(embed=embed)
+            await ctx.reply(embed=embed)
 
 
 @bot.event
